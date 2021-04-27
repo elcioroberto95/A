a4e6163d6d78e8f58d82e6f2cb35f61aa1369b94
elcioroberto95 <elcioroberto1995@gmail.com>
2021-04-27 19:17:14 -0300

    CURSO 4 iniciado

--- CURSO LOGICA 3/Modulo2/sketch.js
+++ CURSO LOGICA 3/Modulo2/sketch.js
@@ -17,6 +17,7 @@ let raqueteAltura = 90;
 let xRaqueteOponente = 585;
 let yRaqueteOponente = 150;
 let velocidadeYOponente;
+let chanceDeErrar = 0;
 
 //variavel boolean de quando colidir
 let colidiu = false;
@@ -24,29 +25,51 @@ let colidiu = false;
 //pontuação
 let meusPontos = 0;
 let pontosDoOponente = 0;
-
+//sons do jogo
+let raquetada;
+let ponto;
+let trilha;
+function preload(){
+trilha = loadSound('./sound/trilha.mp3');
+ponto = loadSound('./sound/ponto.mp3');
+raquetada = loadSound('./sound/raquetada.mp3');
+};
 function colisaoRaquete(x,y){
     let colidiu =    collideRectCircle(x, y, raqueteComprimento, raqueteAltura, xBolinha, yBolinha, raio);
 
     if(colidiu){
         velocidadeXBolinha *= -1;
+        raquetada.play();
     };
 }
+
 function incluiPlacar() {
-    fill(300);
-    text(meusPontos, 278, 30);
-    text(pontosDoOponente, 321, 30);
+    stroke(255);
+    textAlign(CENTER);
+    textSize(16);
+    fill(color(255, 140, 0));
+    rect(150, 10, 40, 20);
+    fill(255);
+    text(meusPontos, 170, 26);
+    fill(color(255, 140, 0));
+    rect(450, 10, 40, 20);
+    fill(255);
+    text(pontosDoOponente, 470, 26);
 }
+
 function marcaPonto() {
     if (xBolinha > 590) {
         meusPontos += 1;
+        ponto.play();
     }
     if (xBolinha < 10) {
         pontosDoOponente += 1;
+        ponto.play();
     }
 }
 function setup() {
     createCanvas(600, 400);
+    trilha.loop();
 };
 function mostraRaquete(xRaquete,yRaquete){
   rect(xRaquete,yRaquete,raqueteComprimento,raqueteAltura);
@@ -63,6 +86,19 @@ function movimentaMinhaRaquete() {
         yRaquete += 10;
     };
 };
+function calculaChanceDeErrar() {
+    if (pontosDoOponente >= meusPontos) {
+      chanceDeErrar += 1
+      if (chanceDeErrar >= 39){
+      chanceDeErrar = 40
+      }
+    } else {
+      chanceDeErrar -= 1
+      if (chanceDeErrar <= 35){
+      chanceDeErrar = 35
+      }
+    }
+  }
 
 function movimentaBolinha(){  
     xBolinha += velocidadeXBolinha;
@@ -71,16 +107,18 @@ function movimentaBolinha(){
 function verificaColisao(){
    if (xBolinha + raio > width || xBolinha - raio < 0) {
         velocidadeXBolinha *= -1;
+    
     };
     if (yBolinha + raio > height || yBolinha - raio< 0) {
         velocidadeYBolinha *= -1;
+      
     };
 };
 function movimentaRaqueteOponente(){
-    velocidadeYOponente = yBolinha - yRaqueteOponente - raqueteComprimento/2 - 30;
-    yRaqueteOponente+=velocidadeYOponente;
-
-};
+    velocidadeYOponente = yBolinha -yRaqueteOponente - raqueteComprimento / 2 - 30;
+    yRaqueteOponente += velocidadeYOponente + chanceDeErrar
+    calculaChanceDeErrar()
+  }
 
 function draw() {
     background(0);
@@ -94,7 +132,7 @@ function draw() {
     colisaoRaquete(xRaquete,yRaquete);
     colisaoRaquete(xRaqueteOponente,yRaqueteOponente);
     incluiPlacar();
-    marcaPonto();
+    marcaPonto()
    };
 
 
